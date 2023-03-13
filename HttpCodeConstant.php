7d875ac9d95baa8f181a58be49d1208f9997bdca
Vincenzo_Jiang <jiangwenchang@ydjdev.com>
2023-03-13 23:11:39 +0800

    first commit

---
+++ HttpCodeConstant.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by : PhpStorm
+ * User: jiangwenchang
+ * Date: 2023/3/13
+ * Time: 11:08 下午
+ */
+
+final class HttpCodeConstant
+{
+    const OK = 200;
+    const SERVER_ERROR = 400;
+    const SERVICE_ERROR = 500;
+}
